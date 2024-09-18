51dea7bab3166503ea4ea010981d05196ae9bdcf
Petertn49 <nyazikapeter@gmail.com>
2024-09-18 10:18:32 +0200

    Created and initialized project with PostgreSQL Lombok and SpringWeb dependencies

---
+++ peter-nyazika-exercise/src/main/java/com/fanset/peter/nyazika/exercise/PeterNyazikaExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.fanset.peter.nyazika.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterNyazikaExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeterNyazikaExerciseApplication.class, args);
+	}
+
+}
---
+++ peter-nyazika-exercise/src/test/java/com/fanset/peter/nyazika/exercise/PeterNyazikaExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanset.peter.nyazika.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeterNyazikaExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
